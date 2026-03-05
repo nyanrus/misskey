51fd09ab5609834cf2c115f55a76ac668fafe50c
nyanrus <68762426+nyanrus@users.noreply.github.com>
2026-03-05 10:32:00 +0900

    aaa

--- packages/backend/src/core/QueueService.ts
+++ packages/backend/src/core/QueueService.ts
@@ -164,11 +164,11 @@ export class QueueService {
 		const label = to.replace('https://', '').replace('/inbox', '');
 
 		return this.deliverQueue.add(label, data, {
-			attempts: this.config.deliverJobMaxAttempts ?? 12,
+			attempts: this.config.nats ? 1 : (this.config.deliverJobMaxAttempts ?? 12),
 			backoff: {
 				type: 'custom',
 			},
-			removeOnComplete: {
+			removeOnComplete: this.config.nats ? true : {
 				age: 3600 * 24 * 7, // keep up to 7 days
 				count: 30,
 			},
